04774b8aa6b8507453d5eec5058db27f60c97289
co-gy <gyc5529@gmail.com>
2024-07-30 05:26:32 +0800

    compare_vi_pi

--- analysis/compare_vi_pi.py
+++ analysis/compare_vi_pi.py
@@ -8,10 +8,10 @@ if __name__ == "__main__":
              root + "policy_iteration/state_value_history(truncate=1).txt",
              root + "policy_iteration/state_value_history(truncate=2).txt",
              root + "policy_iteration/state_value_history(truncate=5).txt",
-             root + "policy_iteration/state_value_history(truncate=1000).txt",
-             root + "mc_basic/state_value_history.txt"]
+             root + "policy_iteration/state_value_history(truncate=100).txt",
+             root + "policy_iteration/state_value_history(truncate=1000).txt"]
     labels = ["vi", "pi(truncate=1)", "pi(truncate=2)",
-              "pi(truncate=5)", "pi(truncate=100)", "pi(truncate=1000)", "mc_basic"]
+              "pi(truncate=5)", "pi(truncate=100)", "pi(truncate=1000)"]
 
     plot = PlotStateValue(state_value_files=files, labels=labels)
     plot.show()
