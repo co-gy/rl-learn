c7f0ceb6989f4864b0b2b219a2b300eddce5d718
co-gy <gyc5529@gmail.com>
2024-07-30 05:11:54 +0800

    Package plotting tools

---
+++ analysis/compare_vi_pi.py
@@ -0,0 +1,17 @@
+import os
+from plot import PlotStateValue
+
+
+if __name__ == "__main__":
+    root = os.getcwd() + "/result/"
+    files = [root + "value_iteration/state_value_history.txt",
+             root + "policy_iteration/state_value_history(truncate=1).txt",
+             root + "policy_iteration/state_value_history(truncate=2).txt",
+             root + "policy_iteration/state_value_history(truncate=5).txt",
+             root + "policy_iteration/state_value_history(truncate=1000).txt",
+             root + "mc_basic/state_value_history.txt"]
+    labels = ["vi", "pi(truncate=1)", "pi(truncate=2)",
+              "pi(truncate=5)", "pi(truncate=100)", "pi(truncate=1000)", "mc_basic"]
+
+    plot = PlotStateValue(state_value_files=files, labels=labels)
+    plot.show()
---
+++ analysis/plot.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from typing import Optional
+
+
+class PlotStateValue(object):
+    def __init__(self, k_range=50, state_value_files: Optional[list]=None, labels: Optional[list]=None):
+        self.k = range(k_range)
+        self.labels = labels
+        self.state_value_list = []
+        for file_name in state_value_files:
+            with open(file_name, "r") as f:
+                self.state_value_list.append(list(map(lambda line: float(line.strip()), f.readlines()[: k_range])))
+    
+    def show(self):
+        for state_value_history, label in zip(self.state_value_list, self.labels):
+            plt.plot(self.k, state_value_history, label=label)
+        plt.legend()
+        plt.xlabel("k")
+        plt.ylabel("state value")
+        plt.show()
+
+
+class RecordStateValue(object):
+    def __init__(self, file_name):
+        self.file = open(file_name, "w")
+        print(file_name)
+        self.state_value_history = ["0\n"]
+    
+    def add(self, state_value):
+        self.state_value_history.append(f"{state_value}\n")
+
+    def __del__(self):
+        self.file.writelines(self.state_value_history)
+        self.file.close()
+
